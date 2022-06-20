31416a8fda3e92d5022408dad14ddba60b24fa35
saraelqady <sarrazee@gmail.com>
2022-06-21 01:15:24 +0200

    initial commit

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -6,7 +6,8 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
-
+  port: config.dbPort,
+  
   dialect: "postgres",
   storage: ":memory:",
 });
